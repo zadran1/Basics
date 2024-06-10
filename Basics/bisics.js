ea81bcb1ffb8adc3d0f00426bff213d37357089f
Samiullah Zadran <samiullahmehdy123@gmail.com>
2024-06-10 15:25:20 +0430

    this is my  first commit

---
+++ Basics/bisics.js
@@ -0,0 +1,10 @@
+
+console.log("hello");
+let a=2
+console.log(a==2);
+//window.alert("Heloo");
+console.log(a==='2');
+console.log(a!=='2');
+console.log(a>3);
+console.log(a < 3);
+console.log(a >= 2);
